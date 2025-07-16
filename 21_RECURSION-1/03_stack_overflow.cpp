711e30c00e21c99d13e807e3689d05c5284c0a8d
amittdas <amitdas123456789@gmail.com>
2025-07-16 23:16:01 +0530

    Added RECURSION-1

---
+++ 21_RECURSION-1/03_stack_overflow.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//segmentation fault
+//missing base case
+
+void func(){
+    cout<<"stack overflow...\n";
+    func();
+}
+int main(){
+    // func();
+return 0;
+}
