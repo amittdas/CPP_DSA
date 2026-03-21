7d1ccd2539d70466f08c798640609705d30fa834
amittdas <amitdas123456789@gmail.com>
2026-03-22 01:04:01 +0530

    Added COUNT DIGITS

---
+++ 11_NUMERICALS/01_Count_digits.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int count(int n){
+    int cnt = 0;
+    while(n > 0){
+        cnt++;
+        n = n / 10;
+    }
+    return cnt;
+}
+
+int main(){
+    int n;
+    cin >> n;
+
+    cout << count(n);
+return 0;
+}
