857f02b0989687ccf85f1049e6032a588eac4a08
amittdas <amitdas123456789@gmail.com>
2026-04-24 16:07:09 +0530

    Added SUM OF ELEMENTS

---
+++ 11_ARRAYS-STRIVER/01_EASY/10_sum_of_all_elements.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int sum(int arr[], int n){
+    int sum = 0;
+    for(int i=0; i<n; i++){
+        sum += arr[i];
+    }
+    return sum ;
+}
+
+int main(){
+    int arr[] = {1,2,3,4,5};
+    cout << sum(arr, 5);
+return 0;
+}
