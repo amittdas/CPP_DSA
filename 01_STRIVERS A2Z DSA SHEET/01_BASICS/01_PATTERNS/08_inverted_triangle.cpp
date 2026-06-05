4183ad378f11d6adf84c9ce7743f1dd950ffb1c2
amittdas <amitdas123456789@gmail.com>
2026-06-05 22:20:11 +0530

    Added inverted Triangle

---
+++ 01_STRIVERS A2Z DSA SHEET/01_BASICS/01_PATTERNS/08_inverted_triangle.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int n){
+    for(int i=0; i<n; i++){
+        //space
+        for(int j=0; j<i; j++){
+            cout<< " ";
+        }
+        //stars
+        for(int j=0; j<(2*n) - (2*i+1); j++){
+            cout << "*";
+        }
+        //space
+        for(int j=0; j<i; j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    print(5);
+return 0;
+}
