7a803a1b9d0dcd5f5c77d0e5dcf4247dcaed7fae
amittdas <amitdas123456789@gmail.com>
2026-06-06 03:12:02 +0530

    Added pattern

---
+++ 01_STRIVERS A2Z DSA SHEET/01_BASICS/01_PATTERNS/20_p.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n-i; j++){
+            cout << "* ";
+        }
+        for(int j=0; j<2*i; j++){
+            cout << "  ";
+        }
+        for(int j=0; j<n-i; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    for(int i=1; i<=n; i++){
+        for(int j=0; j<i; j++){
+            cout << "* ";
+        }
+        for(int j=0; j<(2*n)-(2*i); j++){
+            cout << "  ";
+        }
+        for(int j=0; j<i; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    print(5);
+return 0;
+}
