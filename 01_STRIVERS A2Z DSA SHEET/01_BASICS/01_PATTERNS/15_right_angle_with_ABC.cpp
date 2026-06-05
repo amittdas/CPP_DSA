a533066fcbd3303bbf0dac06bbfeef920c48eb89
amittdas <amitdas123456789@gmail.com>
2026-06-06 03:08:54 +0530

    Added right_angle_with_ABC

---
+++ 01_STRIVERS A2Z DSA SHEET/01_BASICS/01_PATTERNS/15_right_angle_with_ABC.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int n){
+    for(int i=0; i<n; i++){
+        for(char ch = 'A'; ch<='A' + i; ch++){
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    print(5);
+return 0;
+}
